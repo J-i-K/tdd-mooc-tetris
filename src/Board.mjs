e16fbafcd5ea3c3e1adc16c099696336283f7e11
J-i-K <J-i-K@users.noreply.github.com>
2023-03-06 09:49:33 +0000

    board and drop

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,13 +1,20 @@
 export class Board {
   width;
   height;
+  state;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
   }
 
+  state = "...\n...\n...\n";
+
   toString() {
-    return "TODO";
+    return this.state;
+  }
+
+  drop(block) {
+    this.state = ".X.\n...\n...\n";
   }
 }
