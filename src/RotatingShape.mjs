1dcb3d30887baadd41db9d8446afbe100e758735
J-i-K <J-i-K@users.noreply.github.com>
2023-03-10 16:35:19 +0000

    rotating

---
+++ src/RotatingShape.mjs
@@ -0,0 +1,60 @@
+export class RotatingShape {
+  shape;
+
+  constructor(shape) {
+    this.shape = shape.split('\n')
+    .map(string => string.trim())
+  }
+
+  rotateRight() {
+    const origShape = this.shape
+    const newShape = []
+    for (let x in origShape) {
+      const tempArray = []
+      for (let y in origShape[x]) {
+        // console.log(origShape[y][x])
+        tempArray.unshift(origShape[y][x])
+      }
+      newShape.push(tempArray.join(''))
+    }
+    // this.shape = newShape.join('\n').concat('\n')
+    // return this.shape.toString()
+    return newShape.join('\n').concat('\n')
+  }
+
+  rotateLeft() {
+    const origShape = this.shape
+    const newShape = []
+    for (let x in origShape) {
+      const tempArray = []
+      for (let y in origShape[x]) {
+        // console.log(origShape[x])
+        tempArray.push(origShape[y][x])
+        console.log(tempArray)
+      }
+      newShape.unshift(tempArray.join(''))
+    }
+    return newShape.join('\n').concat('\n')
+  }
+
+  toString() {
+    return this.shape.join('\n').concat('\n')
+  }
+
+  // arrayRotate(arr, direction) {
+  //   const unrotatedArray = arr
+  //   const newArray = []
+  //   console.log('array is array?', Array.isArray(unrotatedArray), 'array looks like: ', unrotatedArray)
+  //   if (direction === 'left') {
+  //     unrotatedArray.unshift(unrotatedArray.pop())
+  //     console.log(unrotatedArray)
+  //   }
+  //   if (direction === 'right') {
+  //     // console.log(arr)
+  //     unrotatedArray.push(unrotatedArray.shift())
+  //     // console.log(typeof arr)
+  //   }
+  //   console.log('array is array?', Array.isArray(unrotatedArray), 'array looks like: ', unrotatedArray)
+  //   return unrotatedArray;
+  //   }
+}
