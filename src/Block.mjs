f1c2828ea5a45d407b3d57e8aab4ba47110776aa
J-i-K <J-i-K@users.noreply.github.com>
2023-03-11 23:52:28 +0000

    board size and Tetromino falling

--- src/Block.mjs
+++ src/Block.mjs
@@ -1,7 +1,9 @@
 export class Block {
   color;
+  shape;
 
   constructor(color) {
     this.color = color;
+    this.shape = color;
   }
 }
--- src/Board.mjs
+++ src/Board.mjs
@@ -1,16 +1,22 @@
 export class Board {
   width;
   height;
-  state = this.emptyBoard();
+  state;// = this.emptyBoard();
   blockIsFalling = false;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
-  }
 
-  emptyBoard() {
-    return ["...", "...", "..."];
+    const boardArr = []
+    for (let x = 0; x < this.height; x++) {
+      const tempArray = []
+      for (let y = 0; y < this.width; y++) {
+        tempArray.push('.')
+      }
+      boardArr[x] = tempArray.join('')
+    }
+    this.state = boardArr
   }
 
   toString() {
@@ -22,8 +28,19 @@ export class Board {
   }
 
   drop(block) {
+    const tempState = this.state
     if (!this.blockIsFalling) {
-      this.state[0] = `.${block.color}.`
+      // console.log(block.shape.length)
+      for (let row in block.shape) {
+        for (let sqr in block.shape[row]) {
+          const boardRow = tempState[row].split('')
+          // console.log(this.width,Number(Math.floor(this.width / 2) - Number(sqr)), Number(sqr))//, Number(this.width % 2 + sqr))
+          boardRow[Number(Math.floor(this.width / 2) - Number(sqr))] = block.shape[row][sqr]
+          // console.log(boardRow)
+          this.state[row] = boardRow.join('')
+        }
+      }
+      // this.state[0] = `.${block.color}.`
       this.blockIsFalling = true;
     } else if (this.blockIsFalling) {
       throw new Error("already falling");
@@ -31,38 +48,47 @@ export class Board {
   }
 
   tick() {
-    let startState = this.state
-    // console.log('state at start: ', this.state)
     if (this.blockIsFalling = true) {
-      // console.log('falling')
-      if (startState[2] === '...') {
-        // console.log('no blocks on last row')
-        this.state[2] = startState[1]
-        this.state[1] = startState[0]
-        this.state[0] = '...'
-      } else if (startState[1] === '...') {
-        // console.log('no blocks on middle row')
-        if (startState[0] === '...') {
-          // console.log('nothing to do')
+      const emptyRow = []
+      for (let y = 0; y < this.width; y++) {
+        emptyRow.push('.')
+      }
+      const workState = []
+      for (let row = Number(this.state.length -1); row >= 0; row--) { // FOR EACH ROW, STARTING FROM BOTTOM
+        console.log(row)
+        if (this.state.slice(0, Number(row)).every(function isEmpty(x) {return x === emptyRow.join('')}) && this.state.slice(row).every(function isNotEmpty(x) {return x !== emptyRow.join('')})) {
+          console.log('this.state all above is empty and below is not, nothing to do')
           this.blockIsFalling = false
+          workState[row] = this.state[row]
+        } else if (this.state[row] === emptyRow.join('')) { // IF ROW IS EMPTY, COPY PREVIOUS ROW
+          console.log('empty row')
+          if (row !== 0) {
+            console.log('other than row 0')
+            workState[row] = this.state[Number(row - 1)] // COPY THE PREVIOUS ROW TO CURRENT
+          } else {
+            console.log('row 0')
+            workState[row] = this.state[row]
+          }
+        } else if (this.state[row] !== emptyRow.join('')) {
+          console.log('not empty row')
+          if (row !== 0 && row !== Number(this.state.length - 1)) {
+            console.log('other than row 0 or last row')
+            workState[row] = this.state[Number(row - 1)] // COPY THE PREVIOUS ROW TO CURRENT
+          } else if (row !== 0) {
+            console.log('last row')
+            workState[row] = this.state[row]
+          } else {
+            console.log('row 0')
+            workState[row] = emptyRow.join('')
+          }
+        } else if (workState.slice(0, Number(row)).every(function isEmpty(x) {return x === emptyRow.join('')}) && workState.slice(row).every(function isNotEmpty(x) {return x !== emptyRow.join('')})) {
+          console.log('workstate all above is empty and below is not')
         } else {
-          this.state[1] = startState[0]
-          this.state[0] = '...'
+          console.log('hit else', this.state[row])
         }
       }
-      else if (startState[0] === '...' && (startState[1] !== '...' || startState[2] !== '...')) {
-        // console.log('no new blocks and nothing to move')
-        this.blockIsFalling = false
-      }
-      else if (startState === this.state) {
-        // console.log('no changes')
-        this.blockIsFalling = false
-      }
-      else {
-        // console.log('so no blocks are moving')
-        this.blockIsFalling = false
-      }
-      // console.log('state at end: ', this.state, ' and a block is moving?', this.blockIsFalling)
+      console.log(workState)
+      this.state = workState
     }
   }
 }
