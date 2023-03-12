d310e76c3e67689374de8cb92de6dfd0f9a89eca
J-i-K <J-i-K@users.noreply.github.com>
2023-03-12 13:48:27 +0000

    moves left, right and down

--- src/Board.mjs
+++ src/Board.mjs
@@ -30,26 +30,6 @@ export class Board {
     return this.boardTetrominoes.filter(tetromino => tetromino.isFalling === true).length === 0 ? false : true
   }
 
-  // dropOld(block) {
-  //   const tempState = this.state
-  //   if (!this.blockIsFalling) {
-  //     // console.log(block.shape.length)
-  //     for (let row in block.shape) {
-  //       for (let sqr in block.shape[row]) {
-  //         const boardRow = tempState[row].split('')
-  //         // console.log(this.width,Number(Math.floor(this.width / 2) - Number(sqr)), Number(sqr))//, Number(this.width % 2 + sqr))
-  //         boardRow[Number(Math.floor(this.width / 2) - Number(sqr))] = block.shape[row][sqr]
-  //         // console.log(boardRow)
-  //         this.state[row] = boardRow.join('')
-  //       }
-  //     }
-  //     // this.state[0] = `.${block.color}.`
-  //     this.blockIsFalling = true;
-  //   } else if (this.blockIsFalling) {
-  //     throw new Error("already falling");
-  //   }
-  // }
-
   drop(tetromino) {
     if (!this.hasFalling()) {
       console.log('drop--')
@@ -72,33 +52,6 @@ export class Board {
       throw new Error("already falling");
     }
   }
-
-  // drop(block) {
-  //   let tempState = this.state
-  //   console.log(this.state)
-  //   for (let x in this.state) {
-  //     for (let y in this.state[Number(x)]) {
-  //       tempState[Number(x)][Number(y)] = this.state[Number(x)][Number(y)]
-  //     }
-  //   }
-  //   console.log(tempState)
-  //   // let tempState = this.state.splt('')
-  //   if (!this.blockIsFalling) {
-  //     for (let x in block.shape) {
-  //       for (let y in block.shape[x]) {
-  //         if (block.shape[Number(x)][Number(y)] !== '.') {
-  //           // console.log(tempState[Number(x)][Number(Math.floor(this.width / 2) - Number(y))], block.shape[Number(x)][Number(y)])
-  //           tempState[Number(x)][Number(Math.floor(this.width / 2) - Number(y))] = block.shape[Number(x)][Number(y)]
-  //         }
-  //         // this.state[Number(x)][Number(y)] = boardRow.join('')
-  //       }
-  //     }
-  //     this.blockIsFalling = true;
-  //     console.log(tempState, this.state)
-  //   } else if (this.blockIsFalling) {
-  //     throw new Error("already falling");
-  //   }
-  // }
 
   tick() {
     if (this.hasFalling()) {
@@ -132,82 +85,98 @@ export class Board {
     }
   }
 
-  tickOld() {
-      if (this.hasFalling()) {
-        console.log('tick!')
-        const fallingTetromino = this.boardTetrominoes.filter(tetromino => tetromino.isFalling === true)[0]
-        // console.log(fallingTetromino)
-        // console.log(fallingTetromino.blockList.filter(function(blockA,blockB) { return blockA.x < blockB.x && blockA.y === blockB.y ? blockB : blockA }))
-        for (let block in fallingTetromino.blockList.filter(function(blockA,blockB) {
-          return blockA.x < blockB.x && blockA.y === blockB.y ? blockB : blockA
-        })) {
-          console.log('get the ball rolling', fallingTetromino.blockList[block].x, this.height - 1)
-          // console.log(this.state[0])
-          if (fallingTetromino.blockList[block].x < this.height - 1) {// Not on bottom row
-            console.log('not on bottom row, continuing')
-            if (this.state[Number(fallingTetromino.blockList[block].x) + 1][Number(fallingTetromino.blockList[block].y)] === '.') {
-              console.log('below is empty, hooray!')
-              console.log('this is the below row before update', this.state[fallingTetromino.blockList[block].x + 1])
-              this.state[Number(fallingTetromino.blockList[block].x) + 1][Number(fallingTetromino.blockList[block].y)] = fallingTetromino.shapeStyle
-              this.state[Number(fallingTetromino.blockList[block].x)][Number(fallingTetromino.blockList[block].y)] = '.'
-              console.log('this is the below row after update', this.state[fallingTetromino.blockList[block].x + 1])
-              this.boardTetrominoes[fallingTetromino.id].blockList[block].x++
-            } else { // Below is not empty so we must stop
-              console.log('so we hit this else here')
-              this.boardTetrominoes[fallingTetromino.id].isFalling = false
-            }
-          } else if (this.boardTetrominoes[fallingTetromino.id].blockList[block].x === this.height - 1) { // So this tetromino has reached the bottom
-            this.boardTetrominoes[fallingTetromino.id].isFalling = false
-          } else {
-            console.log('did we end up in here instead?')
-          }
+  moveLeft() {
+    if (this.hasFalling()) {
+      console.log('movingLeft')
+      const movingTetromino = this.boardTetrominoes.filter(tetromino => tetromino.isFalling === true)[0]
+      let tetrominoCanMove = true
+      for (let block in movingTetromino.blockList) {
+        if (movingTetromino.blockList.filter(listBlock => listBlock.y === Number(movingTetromino.blockList[block].y - 1) && listBlock.x === Number(movingTetromino.blockList[block].x)).length !== 0) {
+          console.log('there is another block that is below this one from the same tetromino')
+        } else if (movingTetromino.blockList[block].y - 1 === 0 || // Left side, cannot move
+        this.state[Number(movingTetromino.blockList[block].x)][Number(movingTetromino.blockList[block].y - 1)] !== '.') {// Not empty below, cannot move
+          console.log('hit cannot move for block: ', movingTetromino.blockList[block])
+          tetrominoCanMove = false
+        } else {
+          console.log('else')
         }
+      }
+      if (tetrominoCanMove) {
+        console.log('made it here?')
+        // console.log(this.state)
+        console.log(movingTetromino.blockList.sort(function(a,b){return a.y - b.y}).reverse())
+        movingTetromino.blockList.sort(function(a,b){return a.y - b.y}).map(block => {
+          this.state[Number(block.x)][Number(block.y)] = '.'
+          block.y--
+          this.state[Number(block.x)][Number(block.y)] = movingTetromino.shapeStyle
+        })
+      } else {
+        fallingTetromino.isFalling = false
+      }
+      // console.log('state after update', this.state)
     }
   }
 
-  // tick() {
-  //   if (this.blockIsFalling = true) {
-  //     const emptyRow = []
-  //     for (let y = 0; y < this.width; y++) {
-  //       emptyRow.push('.')
-  //     }
-  //     let workState = []
-  //     for (let row = Number(this.state.length -1); row >= 0; row--) { // FOR EACH ROW, STARTING FROM BOTTOM
-  //       console.log(row)
-  //       if (this.state.slice(0, Number(row)).every(function isEmpty(x) {return x === emptyRow.join('')}) && this.state.slice(row).every(function isNotEmpty(x) {return x !== emptyRow.join('')})) {
-  //         console.log('so', this.state.slice(0, row))
-  //         console.log('this.state all above is empty and below is not, nothing to do')
-  //         this.blockIsFalling = false
-  //         workState[row] = this.state[row]
-  //       } else if (this.state[row] === emptyRow.join('')) { // IF ROW IS EMPTY, COPY PREVIOUS ROW
-  //         console.log('empty row')
-  //         if (row !== 0) {
-  //           console.log('other than row 0')
-  //           workState[row] = this.state[Number(row - 1)] // COPY THE PREVIOUS ROW TO CURRENT
-  //         } else {
-  //           console.log('row 0')
-  //           workState[row] = this.state[row]
-  //         }
-  //       } else if (this.state[row] !== emptyRow.join('')) {
-  //         console.log('not empty row')
-  //         if (row !== 0 && row !== Number(this.state.length - 1)) {
-  //           console.log('other than row 0 or last row')
-  //           workState[row] = this.state[Number(row - 1)] // COPY THE PREVIOUS ROW TO CURRENT
-  //         } else if (row !== 0) {
-  //           console.log('last row')
-  //           workState[row] = this.state[row]
-  //         } else {
-  //           console.log('row 0')
-  //           workState[row] = emptyRow.join('')
-  //         }
-  //       } else if (workState.slice(0, Number(row)).every(function isEmpty(x) {return x === emptyRow.join('')}) && workState.slice(row).every(function isNotEmpty(x) {return x !== emptyRow.join('')})) {
-  //         console.log('workstate all above is empty and below is not')
-  //       } else {
-  //         console.log('hit else', this.state[row])
-  //       }
-  //     }
-  //     console.log(workState)
-  //     this.state = workState
-  //   }
-  // }
+  moveRight() {
+    if (this.hasFalling()) {
+      console.log('movingLeft')
+      const movingTetromino = this.boardTetrominoes.filter(tetromino => tetromino.isFalling === true)[0]
+      let tetrominoCanMove = true
+      for (let block in movingTetromino.blockList) {
+        if (movingTetromino.blockList.filter(listBlock => listBlock.y === Number(movingTetromino.blockList[block].y + 1) && listBlock.x === Number(movingTetromino.blockList[block].x)).length !== 0) {
+          console.log('there is another block that is below this one from the same tetromino')
+        } else if (movingTetromino.blockList[block].y + 1 === 0 || // Left side, cannot move
+        this.state[Number(movingTetromino.blockList[block].x)][Number(movingTetromino.blockList[block].y + 1)] !== '.') {// Not empty below, cannot move
+          console.log('hit cannot move for block: ', movingTetromino.blockList[block])
+          tetrominoCanMove = false
+        } else {
+          console.log('else')
+        }
+      }
+      if (tetrominoCanMove) {
+        console.log('made it here?')
+        // console.log(this.state)
+        console.log(movingTetromino.blockList.sort(function(a,b){return a.y - b.y}).reverse())
+        movingTetromino.blockList.sort(function(a,b){return a.y - b.y}).reverse().map(block => {
+          this.state[Number(block.x)][Number(block.y)] = '.'
+          block.y++
+          this.state[Number(block.x)][Number(block.y)] = movingTetromino.shapeStyle
+        })
+      } else {
+        fallingTetromino.isFalling = false
+      }
+      // console.log('state after update', this.state)
+    }
+  }
+
+  moveDown() {
+    if (this.hasFalling()) {
+      const fallingTetromino = this.boardTetrominoes.filter(tetromino => tetromino.isFalling === true)[0]
+      let tetrominoCanMove = true
+      for (let block in fallingTetromino.blockList) {
+        if ( fallingTetromino.blockList.filter(listBlock => listBlock.x === Number(fallingTetromino.blockList[block].x + 1) && listBlock.y === Number(fallingTetromino.blockList[block].y)).length !== 0) {
+          console.log('there is another block that is below this one from the same tetromino')
+        } else if (fallingTetromino.blockList[block].x === this.height - 1 || // Bottom row, cannot move
+        this.state[Number(fallingTetromino.blockList[block].x) + 1][Number(fallingTetromino.blockList[block].y)] !== '.') {// Not empty below, cannot move
+          console.log('hit cannot move for block: ', fallingTetromino.blockList[block])
+          tetrominoCanMove = false
+        } else {
+          console.log('else')
+        }
+      }
+      if (tetrominoCanMove) {
+        console.log('made it here?')
+        // console.log(this.state)
+        console.log(fallingTetromino.blockList.sort(function(a,b){return a.x - b.x}).reverse())
+        fallingTetromino.blockList.sort(function(a,b){return a.x - b.x}).reverse().map(block => {
+          this.state[Number(block.x)][Number(block.y)] = '.'
+          block.x++
+          this.state[Number(block.x)][Number(block.y)] = fallingTetromino.shapeStyle
+        })
+      } else {
+        fallingTetromino.isFalling = false
+      }
+      // console.log('state after update', this.state)
+    }
+  }
 }
